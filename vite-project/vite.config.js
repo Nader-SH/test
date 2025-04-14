1c8c7882a2ea7bb6bab6d782947d2703a3a89627
Nader shak-shak <nader@thresholdsa.net>
2025-04-14 17:58:27 +0300

    fix deployeddddd

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,
-    port: 4173,
+    host: true,      // Listen on all network interfaces
+    port: 4173,     // Default preview port
+    allowedHosts: 'all'  // Allow requests from any host
   }
 })
